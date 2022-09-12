f4e720d25b6b52d59bf8d4dca2f2726f9b48d890
Ishan <ishanwijes@gmail.com>
2022-09-12 21:05:18 +1000

    student activities completed and main folder added

--- 01-Activities/22-Stu_Linear-Search/Unsolved/index.js
+++ 01-Activities/22-Stu_Linear-Search/Unsolved/index.js
@@ -1,4 +1,12 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
 
 // TODO: Complete the linearSearch function below.
-const linearSearch = (array, element) => {};
+const linearSearch = (array, element) => {
+    array.forEach((entree, i) => {
+        if(i === element) {
+            return i
+        }
+    });
+
+    return -1
+};
